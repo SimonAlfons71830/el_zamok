671da292a696357099cb1f4749292d3841585572
Simona71830 <skombarova.simona@gmail.com>
2022-04-20 20:33:04 +0200

    first commit

---
+++ el_zamok/Board.h
@@ -0,0 +1,16 @@
+/*
+ * Board.h
+ *
+ * Created: 4/20/2022 8:32:40 PM
+ *  Author: Simona
+ */ 
+
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+
+
+
+
+#endif /* BOARD_H_ */
---
+++ el_zamok/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * el_zamok.cpp
+ *
+ * Created: 4/20/2022 8:31:35 PM
+ * Author : Simona
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
